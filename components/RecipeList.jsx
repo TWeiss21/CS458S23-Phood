0da3cccf0a448990c0d88e201790e1deea052489
FawnGuardian <thielea1319@my.uwstout.edu>
2023-02-22 11:48:22 -0600

    Add Recipe Btn
    PHOOD-65 Added an add recipe btn to the recipe list section. Also added a general btn css so everyone can use the same styling of btn when they are adding a new btn.

--- components/RecipeList.jsx
+++ components/RecipeList.jsx
@@ -13,6 +13,9 @@ const RecipeList = () =>{
         <button className="listPlus" id="listPlus">&#43;</button>
       </div>
     </div>
+    <div className="addRecipeBtnContainer" id="addRecipeBtnContainer">
+    <button className="generalBtn addRecipeBtn" id="addRecipeBtn">Add Recipe</button>
+    </div>
   </div>
   )
 }
