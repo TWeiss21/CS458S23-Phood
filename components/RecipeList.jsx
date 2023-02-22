59fbca2623d1b694b57354d1a437cc9b52898b4d
CodePerson <35588954+comessErinaceus@users.noreply.github.com>
2023-02-22 12:47:21 -0600

    Update RecipeList.jsx

--- components/RecipeList.jsx
+++ components/RecipeList.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
-import { WarnMode } from './WarningModal'
 import recipies from '../mocks/Recipes.json'
 
-// for (let i=0; i< recipies.length; i++){
-//   console.log(recipies[i])
-// }
-
-//get the names of recipies
+//get the names of recipies from the larger mock obj
 recipies.forEach(getNames)
 function getNames(current){
   console.log(current['recipe_name'])
@@ -25,7 +20,6 @@ const RecipeList = () =>{
         <button className="listPlus" id="listPlus">&#43;</button>
       </div>
     </div>
-   <WarnMode name='Delete' message='Are you sure you would like to delete this item? We will not be able to retrieve it.'/>
   </div>
   )
 }
