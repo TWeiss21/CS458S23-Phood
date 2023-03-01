92676224db1fde598f48f411c36059790ad2bdb1
Jacob.Gibbs <Gibbsj12345@outlook.com>
2023-03-01 12:12:25 -0600

    can't use db connection from pages

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,28 @@
 import Dashboard from '../components/dashboard.jsx'
 
-import sqlite3 from 
+//const sqlite3 = require('sqlite3')
 
 // const database = new sqlite3.Database('../sqlitedb/phoddb.sqlite')
 
 // export const getStaticProps = async () => {
-//     const res = await fetch('../mocks/Recipes.json')
-//     console.log(res)
+//     const res = await fetch('../sqlitedb/phooddb.sqlite')
+//     // console.log(res)
 //     const data = await res.json()
 
 //     return {
 //         props: { recipies: data}
 //     }
 // }
-// { recipies } - put in curly
+// { recipies } - put in braces
 const Index = () => (
-    // <div>
-         <Dashboard/>
+    //  <div>
+    //      <Dashboard/>
     //     {recipies.map(recipe => <div key={recipe.id}>
 
-    //         {recipe.name}
+    //          {recipe.name}
     //     </div>)}
-    // </div>
+    //  </div>
+    <Dashboard/>
 )
 
 export default Index
