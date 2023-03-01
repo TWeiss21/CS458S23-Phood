f610420c14fdb527be39232e400a00527a27ce23
FawnGuardian <thielea1319@my.uwstout.edu>
2023-03-01 13:19:00 -0600

    Buttons on recipe detail
    PHOOD-55 Added buttons on the recipe detail view, tweaked some CSS overall. Additional CSS tweaking will need to happen when it comes to the image.

--- components/RecipeDetail.jsx
+++ components/RecipeDetail.jsx
@@ -7,16 +7,21 @@ const RecipeDetail = () =>{
   return(
     <React.Fragment>
       <div className="recipeDetailContainer">
+        <div className="btnHolder">
+          <button className="generalBtn recipeBtn">Add to List</button>
+          <div className="rightAlign">
+            <button className="generalBtn recipeBtn recipeDeleteBtn">Delete</button>
+            <button className="generalBtn recipeBtn recipeEditBtn">Edit</button>
+          </div>
+        </div>
         <div className="recipeDetailLayout">
           <div className='imageGrid'>
-            <div className='image'>image</div>
-            {/*<Image className='image' src={ChickenNug} alt='Chicken Nugs'/>{/*Make image scale */}
+            <Image className='image' src={ChickenNug} alt='Chicken Nugs'/>{/*Make image scale */}
           </div>
           <h1 className='nameGrid'>name
             <div className="recipeListLine recipeDetailLineHeader" id="recipeDetailLineHeader"></div>
           </h1>
           <div className='descGrid'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
-          {/* <div className="ingred"> */}
             <div className='ingredGrid'>
               <div className='ingredientLayout'>
                 <div className="ingredsec sec1"><div>sec1 full text aaa</div><div>sec1 full text aaa</div><div>sec1 full text aaa</div><div>sec1 full text aaa</div><div>sec1 full text aaa</div></div>
@@ -27,7 +32,7 @@ const RecipeDetail = () =>{
           {/* </div> */}
           <div className='stepGrid'>
           <div className="recipeListLine recipeDetailLineSteps" id="recipeDetailLineSteps"></div>
-            <div className="step">1)  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut</div>
+            <div className="step">1) Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut</div>
             <div className="step">2) Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut</div>
             <div className="step">3) Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut</div>
             <div className="step">4) Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut</div>
