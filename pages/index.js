fba3137f879d7fe22d01800fab4a8ed338ee25bf
AndrewUjda <drewjthom@gmail.com>
2023-03-06 15:05:38 -0600

    Testing Modal
    Phood-5

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Dashboard from '../components/dashboard.jsx'
+import AddRecipeModal from '@/components/AddRecipeModal.jsx'
 
 
 const Index = () => (
     <div>
         <Dashboard/>
+        <AddRecipeModal/>
     </div>
 )
 
