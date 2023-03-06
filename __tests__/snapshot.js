8df03a2c9d8530871cd5aa733494b97a6619040b
TWeiss21 <weisst2468@gmail.com>
2023-03-06 12:59:09 -0600

    created updated snapshot

--- __tests__/snapshot.js
+++ __tests__/snapshot.js
@@ -1,10 +1,8 @@
 import { Dashboard } from '@mui/icons-material'
-import { render } from "@testing-library/react"
+import { render } from '@testing-library/react'
 import { Index, Home } from '../pages/index'
 
-describe('renders homepage unchanged', () => {
-    it("Matches DOM Snapshot", () => {
-        const { container } = render(<Index/>);
-        expect(container).toMatchSnapshot();
-    });
+it("renders homepage unchanged", () => {
+    const { container } = render(<Dashboard/>);
+    expect(container).toMatchSnapshot();
 });
