e21a17fee676c38fb60a09b2f721db4afa95c304
FawnGuardian <thielea1319@my.uwstout.edu>
2023-03-22 14:41:35 -0500

    List added
    PHOOD-80 list has been added to shopping list section. General block out of list. Slight CSS tweaks are needed and interactive visuals are needed. The qty section needs to be fleshed out as well.

--- components/ShoppingList.jsx
+++ components/ShoppingList.jsx
@@ -4,9 +4,25 @@ const ShoppingList = () =>{
   return(
   <div className="shopGrid">
     <div className="shopList">
-      Shopping List
+      <div className="shopHeader">Shopping List</div>
+      <div className="shopListContainer">
+        <div className="ingredItem">
+          <div className="verticalCentered">
+            <div className="ingredName">Chick</div>
+            <div className="qty">qty</div>
+            <div className="remove">x</div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div className="shopBtns">Btns</div>
+    <div className="shopBtns">
+      <div className="verticalCentered">
+        <div className="btnSpace centered"><button className="generalBtn shopInteractiveBtns">Clear</button></div>
+        <div className="btnSpace centered"><button className="generalBtn shopInteractiveBtns">Undo</button></div>
+        <div className="btnSpace centered"><button className="generalBtn shopInteractiveBtns">Export</button></div>
+      </div>
+    </div>
+    
   </div>
   )
 }
