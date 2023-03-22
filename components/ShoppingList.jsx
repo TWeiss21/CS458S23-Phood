cdea6e36350c1f52548a4e6cacf8779610b92903
FawnGuardian <thielea1319@my.uwstout.edu>
2023-03-22 12:59:58 -0500

    Grid layout for Shopping List
    PHOOD-80 Pantry has been changed to be the shopping list.

---
+++ components/ShoppingList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ShoppingList = () =>{
+  return(
+  <div className="shopGrid">
+    <div className="shopList">
+      Shopping List
+    </div>
+    <div className="shopBtns">Btns</div>
+  </div>
+  )
+}
+
+export default ShoppingList
--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "../styles/dashboard.css";
 import "../styles/header.css";
 import "../styles/recipeList.css";
 import "../styles/recipeDetails.css";
+import "../styles/shoppingList.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
