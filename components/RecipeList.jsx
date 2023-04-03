3b230696e7ba1219166c764df8943ccb0d79570a
FawnGuardian <thielea1319@my.uwstout.edu>
2023-04-03 09:34:06 -0500

    Remove div to fix breaking recipe list

--- components/RecipeList.jsx
+++ components/RecipeList.jsx
@@ -23,16 +23,16 @@ export class GetRecipeNames extends Component {
                       <button key={list.id} className="recipeContainer" id="recipeContainer">
                         <div className="recipeListName" id="recipeListName">{list.recipe_name}</div>
                         <div className="recipeListLine" id="recipeListLine"></div>
-                      </button><button className="listPlus" id="listPlus">&#43;</button>
+                      </button>
+                      <button className="listPlus" id="listPlus">&#43;</button>
                     </div>
                     </>
                   ))
                 }
             </div>
-        </div>
-        <div className="addRecipeBtnContainer" id="addRecipeBtnContainer">
-        <button className="generalBtn addRecipeBtn" id="addRecipeBtn"><AddRecipeModal/></button>
-        </div>
+          <div className="addRecipeBtnContainer" id="addRecipeBtnContainer">
+          <button className="generalBtn addRecipeBtn" id="addRecipeBtn"><AddRecipeModal/></button>
+          </div>
         </div>
       )
   }
