0b7714b528b3bf2dcafcfa40807a0232f04ab369
TWeiss21 <weisst2468@gmail.com>
2023-04-06 15:10:31 -0500

    updated post

--- pages/api/postToPantry.js
+++ pages/api/postToPantry.js
@@ -3,7 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 //import _req from 'components/AddRecipeModal'
 
 const db = new sqlite3.Database('./sqlitedb/phooddb.sqlite');
-const query = 'SELECT * FROM pantry';
+const query = 'SELECT EXISTS(SELECT 1 FROM pantry WHERE name=ingr_data[0].name';
 const push = 'INSERT INTO pantry (name) VALUES (?)';
 //const rows = db.prepare(query);
 
@@ -13,38 +13,42 @@ export default async function handlerIngr(_req, res) {
     var ingr_data = JSON.parse(_req.body);
 
     //const itemSet = new Set();
-    let data = [];
 
     console.log(ingr_data);
 
-    db.all(query, [], (err, rows)=>{
-      if(err){throw err}
-      rows.forEach((row) => {
-        data.push(row);
-        const specificRow = data.find(row => row.name === ingr_data);
-        if (!specificRow) {
-          console.log(row),
-          push,
-          ingr_data,
-          function (err) {
+    db.run('INSERT INTO pantry (name) VALUES (?)',
+          ingr_data[0].name,
+            function (err) {
             if (err) {
               console.error(err);
               res.status(500).json({ error: 'Something went wrong' });
             } else {
               res.status(200).json({ id: this.lastID });
             }
-          }
-        }
-        else {
-          return;
-        }
-      })
-    })
+            }
+         ) 
+    }
+  }
+
+    // db.all(query, [], (err, rows)=>{
+    //   if(err){throw err}
+    //   rows.forEach((row) => {
+    //     data.push(row);
+    //     const specificRow = data.find(row => row.name === ingr_data);
+    //     if (!specificRow) {
+    //       console.log(row);
+    //       push,
+    //       ingr_data,
+          
+    //     }
+    //     else {
+    //       return;
+    //     }
+    //   })
+    // })
       // if (itemSet.has(row.item)) {
       //   console.log('Duplicate item found!');
       //   return;
       // } else {
         
       // }
-    }
-  };
