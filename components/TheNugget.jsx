a00701dcd9617d1b7a074a92c72ec1ee08f7ccf4
Jacob.Gibbs <Gibbsj12345@outlook.com>
2023-04-10 17:04:36 -0500

    passing correct info to the new nugget component

--- components/TheNugget.jsx
+++ components/TheNugget.jsx
@@ -3,7 +3,7 @@ const Nugget = (props)=>{
   return(
   <div className="outerRecipeContainer"id="outerRecipeContainer">
   <button className="recipeContainer" id="recipeContainer" onClick={props.onClick}>
-    <div className="recipeListName" id="recipeListName"></div>
+    <div className="recipeListName" id="recipeListName">{props.recipeName}</div>
     <div className="recipeListLine" id="recipeListLine"></div>
   </button><button className="listPlus" id="listPlus">&#43;</button>
 </div>
