6297b252bfc8a39d71f2457603d5fb9493b4d7a5
AndrewUjda <drewjthom@gmail.com>
2023-04-26 15:05:12 -0500

    Nugget Gone

--- __tests__/components/recipeList.test.jsx
+++ __tests__/components/recipeList.test.jsx
@@ -2,7 +2,6 @@
 
 import { fireEvent, render, screen } from '@testing-library/react'
 import RecipeList from '../../components/RecipeList'
-import Nugget from '@/components/TheNugget'
 import '@testing-library/jest-dom'
 import Nugget from '@/components/TheNugget'
 
