e18d5e944377225414a934914d4cf967646958c5
Jacob.Gibbs <Gibbsj12345@outlook.com>
2023-05-03 01:55:01 -0500

    Deleting items from shopping list now includes warning model

--- components/ShoppingList.jsx
+++ components/ShoppingList.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
+import WarnMode from './WarnMode'
 
 const ShoppingList = (props) =>{
-  function clearAllIngs(){
-    
-    //This works to delete item 29
-    // fetch('http://localhost:3000/api/deleteSLItem',{
-    //   method:'POST',
-    //   headers:{
-    //     'Content-Type':'application/json'
-    //   },
-    //   body: JSON.stringify({id:29})
-    // })
+
+  /** 
+   * clearAllIngs will map through props.listOfIngredients deleting each item based on thir id on the Clear button onClick event
+   * no inputs, deletes table.
+  */
+  function clearAllSL(){
     
     props.listOfIngredients.map(list=>{
       console.log(list)
@@ -22,6 +19,12 @@ const ShoppingList = (props) =>{
       },
       body: JSON.stringify({id:list.id})
     })
+      // TODO: 
+      //1. Try using only one fetch insted of fetch for each item. -Dedicate separate endpoint to clear the table.
+      //2. How could we secure that endpoint?
+      //3. Warning Model could use a touch up.
+      //4. Refresh should be automatic.
+      //5. Ids in db keep counting up instead of resetting upon clear. mildly annoying.
       // if an item already exists in the shopping list, it should not be added in duplicate in the first place.
     })
   }
@@ -64,7 +67,13 @@ const ShoppingList = (props) =>{
     {/* These are the buttons Clear, Undo, Export from the top down */}
     <div className="shopBtns">
       <div className="verticalCentered">
-        <div className="btnSpace centered"><button className="generalBtn shopInteractiveBtns" onClick={clearAllIngs}>Clear</button></div>
+        {/* <div className="btnSpace centered"><button className="generalBtn shopInteractiveBtns" onClick={clearAllIngs}>Clear</button></div> */}
+        <WarnMode 
+        name="Clear" 
+        message="This action will remove all items from the shopping list. Will you continue?"
+        affirmitiveAction = "Continue"
+        negativeAction = "Cancel" 
+        functionWarnedAgainst = {clearAllSL}/>
         <div className="btnSpace centered"><button className="generalBtn shopInteractiveBtns">Undo</button></div>
         <div className="btnSpace centered"><button className="generalBtn shopInteractiveBtns">Export</button></div>
       </div>
