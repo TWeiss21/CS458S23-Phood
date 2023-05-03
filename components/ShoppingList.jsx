36b527e3a5b742d6c17cafb2a71bef6a22bc1485
FawnGuardian <thielea1319@my.uwstout.edu>
2023-05-03 10:02:46 -0500

    Revert "Clear Button works as expected front to back for Shopping List"

--- components/ShoppingList.jsx
+++ components/ShoppingList.jsx
@@ -1,33 +1,6 @@
 import React from 'react'
-import WarnMode from './WarnMode'
 
 const ShoppingList = (props) =>{
-
-  /** 
-   * clearAllIngs will map through props.listOfIngredients deleting each item based on thir id on the Clear button onClick event
-   * no inputs, deletes table.
-  */
-  function clearAllSL(){
-    
-    props.listOfIngredients.map(list=>{
-      console.log(list)
-      
-      fetch('http://localhost:3000/api/deleteSLItem',{
-      method:'POST',
-      headers:{
-        'Content-Type':'application/json'
-      },
-      body: JSON.stringify({id:list.id})
-    })
-      // TODO: 
-      //1. Try using only one fetch insted of fetch for each item. -Dedicate separate endpoint to clear the table.
-      //2. How could we secure that endpoint?
-      //3. Warning Model could use a touch up.
-      //4. Refresh should be automatic.
-      //5. Ids in db keep counting up instead of resetting upon clear. mildly annoying.
-      // if an item already exists in the shopping list, it should not be added in duplicate in the first place.
-    })
-  }
   return(
   <div className="shopGrid">
 
@@ -67,13 +40,7 @@ const ShoppingList = (props) =>{
     {/* These are the buttons Clear, Undo, Export from the top down */}
     <div className="shopBtns">
       <div className="verticalCentered">
-        {/* <div className="btnSpace centered"><button className="generalBtn shopInteractiveBtns" onClick={clearAllIngs}>Clear</button></div> */}
-        <WarnMode 
-        name="Clear" 
-        message="This action will remove all items from the shopping list. Will you continue?"
-        affirmitiveAction = "Continue"
-        negativeAction = "Cancel" 
-        functionWarnedAgainst = {clearAllSL}/>
+        <div className="btnSpace centered"><button className="generalBtn shopInteractiveBtns">Clear</button></div>
         <div className="btnSpace centered"><button className="generalBtn shopInteractiveBtns">Undo</button></div>
         <div className="btnSpace centered"><button className="generalBtn shopInteractiveBtns">Export</button></div>
       </div>
